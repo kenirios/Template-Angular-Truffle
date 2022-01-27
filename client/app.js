04bbe1a4f60f0ece39242ade883a3cc7893eabea
Keni Rios <kenirios11@egmail.com>
2022-01-26 20:37:40 -0500

    Update Conexion y MINT

--- client/app.js
+++ client/app.js
@@ -6,18 +6,22 @@ App = {
     await App.loadContract();
     await App.render();    
   },
+
   loadWeb3: async () => {
     if (window.ethereum) {
       App.web3Provider = window.ethereum;
       await window.ethereum.request({ method: "eth_requestAccounts" });
+      //alert('estoy aqui');
     } else if (web3) {
       web3 = new Web3(window.web3.currentProvider);
+      //alert('estoy aca');
     } else {
       console.log(
         "MetaMask no esta instaldo, por favor instale metamas para Jugar"
       );
     }
   },
+
   loadAccount: async () => {
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
@@ -25,19 +29,36 @@ App = {
     App.account = accounts[0];
     console.log(App.account = accounts[0]);
   },
+
   loadContract: async () => {
     try {
       const res = await fetch("LipToken.json");
       const lipsContractJSON = await res.json();
       App.contracts.LipsContract = TruffleContract(lipsContractJSON);
       App.contracts.LipsContract.setProvider(App.web3Provider);      
-      App.lipsContract = await App.contracts.LipsContract.deployed();
+      App.lipsContract = await App.contracts.LipsContract.deployed();      
     } catch (error) {
       console.error(error);
     }
   },
+
   render: async () => {
-    document.getElementById("account").innerText = App.account;
+    document.getElementById("account").innerText = App.account;    
   },
   
+
+  createRandomLip: async (nombre, cant) => {
+    const valueInWei = 1000000000000000000;
+    try {
+      const result = await App.lipsContract.createRandomLip(nombre, cant, {
+        from: App.account,    
+        value: valueInWei    
+      });
+      console.log(result.logs[0].args);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
 };
--- client/ui.js
+++ client/ui.js
@@ -2,14 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   App.init();
 });
 
-/**
- * Task form
- */
 const mintForm = document.querySelector("#mintForm");
-
-mintForm.addEventListener("submit", (e) => {
+mintForm.addEventListener("submit", (e) => {  
   e.preventDefault();
-  const title = mintForm["title"].value;
-  const description = mintForm["description"].value;
-  App.createTask(title, description);
+  const nombre = mintForm["nombre"].value;
+  const cant = mintForm["cant"].value;
+  App.createRandomLip(nombre, cant);  
 });
